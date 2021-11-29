55fee83f6b30e3149644f57a06b7dde46980d7e8
Emrah SARICICEK <emrah.saricicek@securist.com.tr>
2021-11-29 18:30:29 +0300

    First Commit

---
+++ AI_Analiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AI_Analiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MLModel3_WebApi1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ML;
+namespace MLModel3_WebApi1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Configuration
+            WebHost.CreateDefaultBuilder()
+          .ConfigureServices(services =>
+          {
+                    // Register Prediction Engine Pool
+                    services.AddPredictionEnginePool<MLModel3.ModelInput, MLModel3.ModelOutput>().FromFile("MLModel3.zip");
+          })
+          .Configure(options =>
+          {
+              options.UseRouting();
+              options.UseEndpoints(routes =>
+              {
+                        // Define prediction endpoint
+                        routes.MapPost("/predict", PredictHandler);
+              });
+          })
+          .Build()
+          .Run();
+        }
+
+        static async Task PredictHandler(HttpContext http)
+        {
+            // Get PredictionEnginePool service
+            var predictionEnginePool = http.RequestServices.GetRequiredService<PredictionEnginePool<MLModel3.ModelInput, MLModel3.ModelOutput>>();
+
+            // Deserialize HTTP request JSON body
+            var body = http.Request.Body as Stream;
+            var input = await JsonSerializer.DeserializeAsync<MLModel3.ModelInput>(body);
+
+            // Predict
+            MLModel3.ModelOutput prediction = predictionEnginePool.Predict(input);
+
+            // Return prediction as response
+            await http.Response.WriteAsJsonAsync(prediction);
+        }
+    }
+}
+
